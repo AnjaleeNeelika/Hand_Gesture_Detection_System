985a21219b4a5cce590b724fba6cb5221607f405
R.A.T.U.Perera <92754215+thirani99@users.noreply.github.com>
2024-02-05 15:48:05 +0530

    resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './assets/pages/Home'
 import Reports from './assets/pages/Reports'
 import GetCount from './assets/pages/GetCount'
+import VideoUploadPage from './pages/VideoUploadPage'
+import VideoUploadPageeee from './pages/VideoUploadPageeee'
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
           <Route path='/home' element={<Home />} />
           <Route path='/reports' element={<Reports />} />
           <Route path='/get-count' element={<GetCount />} />
+          <Route path='/videoUploadPage' element={<VideoUploadPage />} />
+          <Route path='/uploadVideo' element={<VideoUploadPageeee />} />
         </Routes>
       </div>
     </>
