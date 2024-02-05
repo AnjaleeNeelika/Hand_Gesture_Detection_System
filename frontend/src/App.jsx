2424c203e54bf56680d59d0740723b8ee6b95d03
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2024-02-05 15:36:10 +0530

    Modified Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Navbar from './assets/components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './assets/pages/Home'
+import Reports from './assets/pages/Reports'
+import GetCount from './assets/pages/GetCount'
 
 const App = () => {
     return (
         <>
             <Navbar />
-            <div className='bg-[#cfb5b54d] w-full h-screen'></div>
-        </>        
+            <div className='bg-[#f3e4e4] w-full h-screen pt-20'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/reports' element={<Reports />} />
+                    <Route path='/get-count' element={<GetCount />} />
+                </Routes>
+            </div>
+        </>
     )
 }
 
